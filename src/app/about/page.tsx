2e1a0b1d622da49ac522909e35c9b8392afca6ce
Ahmedmalik <159722798+ahmed22138@users.noreply.github.com>
2025-02-04 03:51:46 +0500

    up

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,85 +1,21 @@
-"use client";
 
-
-import { client } from "@/sanity/lib/client"
-import { allChef } from "@/sanity/lib/queries"
-import { useEffect, useState } from "react"
-import { Chef } from "../../../types/food"
 import Image from "next/image";
-import { ImageResponse } from "next/server";
-import { urlFor } from "@/sanity/lib/image";
-import Navbar from "@/components/nav";
 
 
 
-
-
-
-const Chefs = () => {
-
-    const [chef , setFood] = useState<Chef[]>([])
-
-    useEffect (() => {
-
- async function fetchchef() {
-
-    const fetchedChef : Chef[] =await client.fetch(allChef)
-    setFood(fetchedChef)
- }
- fetchchef()
-
-    },[])
-
+export default function About(){
     return(
         <div>
-        
-           <Navbar/>
-              <div className="mtts">
-        
-               <Image src={'/images/dc.png'} alt="" width={1920} height={410}/>
-              
-                    <h1>Our Chefs</h1>
-                    <h2>Home &gt; <span> Chefs </span></h2>
-                    </div>
-        
-
-        <div className="max-w-5xl mx-auto px-20 py-16 ">
-<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 py-6 gap-10 ">
-
-{
-    chef.map((chef)=>(
-        <div key={chef._id}>
-            {chef.image &&
-          <Image
-          src={urlFor(chef.image).url()}
-          alt="image"
-          width={200}
-          height={200}
-          className="w-full"
-          />
-            }
-         <h1 className="text-xl font-serif py-1">{chef.name}</h1>
-         <p className="pb-6 ">{chef.description}</p>
-        </div>
-
-    ))
-}
-
-
-
-</div>
-</div>
-
-
- <div>
-            <Image src={'/images/Component 75.png'} alt="//" width={1923} height={740}/>
+            <div className="md:ml-36 md:mt-36">
+              <Image src={'/images/Image (1).png'} alt={'pics'} width={600} height={700}  />
             </div>
-
-</div>
-
-
-
+            <div className="md:ml-[54vw] md:-mt-[30vw] md:w-[40vw]">
+            <Image src={'/images/Line.png'} alt={'pics'} width={100} height={160}  />
+            <h1 className="md:mt-5 md:font-bold md:text-5xl">Food is an important part of a Balanced Diet </h1>
+            <p className="md:mt-5 md:text-xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam praesentium, aspernatur ullam ipsa quis sit tenetur minus deleniti, earum accusantium qui velit dolores alias sequi est porro totam inventore? Culpa!
+            Mollitia tempore beatae incidunt laudantium quae tenetur pariatur, repudiandae molestiae iste! Alias natus dolore laudantium velit temporibus perferendis. Illo cum reiciendis accusamus unde? Natus, maxime sed. Facilis a eos assumenda?</p>
+           <button className="md:w-44 md:h-10 md:bg-amber-500 md:mt-5 md:rounded-md">Show More</button>
+                </div>
+        </div>
     )
-
 }
-export default Chefs
