1575cb699d98872b1aa25168c987a3328af55bd8
Ahmedmalik <159722798+ahmed22138@users.noreply.github.com>
2025-02-04 04:33:17 +0500

    up

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ export default function About(){
         <div>
             <div className="md:ml-36 md:mt-36">
               <Image src={'/images/Image (1).png'} alt={'pics'} width={600} height={700}  />
-              <Image src={'/images/mlo.png'} alt={'pics'} width={1920} height={160} className="md:absolute top-[76vw] left-0" />
+              <Image src={'/images/mlo.png'} alt={'pics'} width={1920} height={160} className="md:absolute top-[78vw] left-0" />
               <Image src={'/images/best chef.png'} alt={'pics'} width={400} height={160} className="md:absolute top-[110vw] left-24" />
               <Image src={'/images/120 Item.png'} alt={'pics'} width={400} height={160} className="md:absolute top-[110vw] left-[36vw]" />
               <Image src={'/images/Clean environment.png'} alt={'pics'} width={400} height={160} className="md:absolute top-[110vw] left-[65vw]" />
@@ -30,7 +30,7 @@ export default function About(){
            <Image src={'/images/Video Button.png'} alt={'pics'} width={100} height={160} className="md:ml-60 md:-mt-9 " />
 
              <h1 className="md:mt-80 md:-ml-52 md:text-5xl md:font-semibold">Why Choose us</h1>
-             <p className="md:-ml-72 md:w-[35vw] md:mt-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero consectetur facilis, debitis aperiam, blanditiis dolorum fuga, nobis eius labore illum nisi .</p>
+             <p className="md:-ml-72 md:w-[35vw] md:mt-5 md:mb-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero consectetur facilis, debitis aperiam, blanditiis dolorum fuga, nobis eius labore illum nisi .</p>
                 </div>
         </div>
     )
